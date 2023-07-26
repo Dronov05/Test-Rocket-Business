439ea866dec5985388e97072896d003a239b3c65
Max <23432-az@mail.ru>
2023-07-26 21:44:08 +0300

    Добавил слушатель событий для кнопки гамбургер

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-const swiper = new Swiper(".swiper", {
-   pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-   },
-   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-   },
-});
+// const swiper = new Swiper(".swiper", {
+//    pagination: {
+//       el: ".swiper-pagination",
+//       type: "fraction",
+//    },
+//    navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//    },
+// });
+
+const menuBtn = document.querySelector('.header__btn')
+const geolocStreet = document.querySelector('.header__geoloc-street')
+const headerBtn = document.querySelector('.header__button')
+const headerMenu = document.querySelector('.header__bg-bottom')
+
+menuBtn.addEventListener('click', () => {
+   menuBtn.classList.toggle('header__btn--active')
+   headerMenu.classList.toggle('header__bg-bottom--active')
+   geolocStreet.classList.toggle('header__geoloc-street--active')
+   headerBtn.classList.toggle('header__button--active')
+})
