e1f260d3e48369a66fe7e53c85a1258ab0fef19a
Max <23432-az@mail.ru>
2023-07-27 22:56:50 +0300

    Поменял местами код для гамбургера и swiper

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-// const swiper = new Swiper(".swiper", {
-//    pagination: {
-//       el: ".swiper-pagination",
-//       type: "fraction",
-//    },
-//    navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//    },
-// });
-
-const menuBtn = document.querySelector('.header__btn')
+const menuBtn = document.querySelector('.menu__btn')
 const geolocStreet = document.querySelector('.header__geoloc-street')
 const headerBtn = document.querySelector('.header__button')
 const headerMenu = document.querySelector('.header__bg-bottom')
 
 menuBtn.addEventListener('click', () => {
-   menuBtn.classList.toggle('header__btn--active')
+   menuBtn.classList.toggle('menu__btn--active')
    headerMenu.classList.toggle('header__bg-bottom--active')
-   geolocStreet.classList.toggle('header__geoloc-street--active')
-   headerBtn.classList.toggle('header__button--active')
 })
+
+const swiper = new Swiper(".swiper", {
+   pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+   },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+});
