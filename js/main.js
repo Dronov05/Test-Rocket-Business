13295eb1f32ae35d5d85b613a68775702e9f3518
Max <23432-az@mail.ru>
2023-07-29 15:46:03 +0300

    Добавил слушатели событий для закрывания модального окна вслывающей формы

--- js/main.js
+++ js/main.js
@@ -2,19 +2,45 @@ const body = document.body
 const menuBtn = document.querySelector('.menu__btn')
 const headerMenu = document.querySelector('.header__bg-bottom')
 
+const headerBtn = document.getElementById('header-btn')
+const checkUpBtn = document.getElementById('check-up-btn')
+const mobBtn = document.getElementById('mob-btn')
+const modalOpen = document.querySelector('.modal__overlay')
+const modalClose = document.querySelector('.modal__close')
+const modalBtnClose = document.getElementById('modal-btn-close')
+
 menuBtn.addEventListener('click', () => {
    menuBtn.classList.toggle('menu__btn--active')
    headerMenu.classList.toggle('header__bg-bottom--active')
    body.classList.toggle('noscroll')
 })
 
-headerMenu.querySelectorAll('menu__list-link').forEach(link => {
-   link.addEventListener('click', () => {
-      menuBtn.classList.remove('menu__btn--active')
-      headerMenu.classList.remove('header__bg-bottom--active')
-      body.classList.remove('noscroll')
-   })
+
+headerBtn.addEventListener('click', () => {
+   modalOpen.classList.add('open')
 })
+
+checkUpBtn.addEventListener('click', () => {
+   modalOpen.classList.add('open')
+})
+
+mobBtn.addEventListener('click', () => {
+   modalOpen.classList.add('open')
+})
+
+modalBtnClose.addEventListener('click', () => {
+   modalOpen.classList.remove('open')
+})
+
+modalClose.addEventListener('click', () => {
+   modalOpen.classList.remove('open')
+})
+
+window.onclick = e => {
+   if (e.target == modalOpen) {
+      modalOpen.classList.remove('open')
+   }
+}
 
 const swiper = new Swiper(".swiper", {
    pagination: {
